da1653946f80496dedd43d7345f340c87d3f4c17
Romanos Papanikolaou <romanos.papanikolaou@gmail.com>
2025-03-18 10:29:03 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
 import { ItemListComponent } from './components/item-list/item-list.component';
 
 @NgModule({
-  declarations: [AppComponent, AddItemComponent, ItemListComponent],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    AddItemComponent,
+    ItemListComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
